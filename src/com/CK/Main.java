70cf99855a6a20f24d61cf2307d963f0fdac625c
CK <kainingc@usc.edu>
2019-07-17 16:22:57 -0700

    Add new Codec2

--- src/com/CK/Main.java
+++ src/com/CK/Main.java
@@ -34,7 +34,7 @@ public class Main {
 //        node6.left = node13;
 //        node6.right = node14;
 
-        Codec solution = new Codec();
+        Codec2 solution = new Codec2();
         System.out.println(solution.serialize(root));
         solution.deserialize(solution.serialize(root)).printTree();
     }
@@ -75,7 +75,6 @@ class Codec {
                 sb.append("e,");
             } else {
                 sb.append(node.val + ",");
-//                if ((node.right == null) && (node.left == null)) return;
                 if (node.left != null) {
                     bfsQueue.offer(node.left);
                     bfsChar.offer(String.valueOf(node.left.val));
@@ -123,5 +122,48 @@ class Codec {
         String res = sb.substring(0, firstComma);
         sb.delete(0, firstComma + 1);
         return res;
+    }
+}
+
+// BFS
+class Codec2 {
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                res.append("n ");
+                continue;
+            }
+            res.append(node.val + " ");
+            q.add(node.left);
+            q.add(node.right);
+        }
+        return res.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        String[] values = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        q.add(root);
+        for (int i = 1; i < values.length; i++) {
+            TreeNode parent = q.poll();
+            if (!values[i].equals("n")) {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left = left;
+                q.add(left);
+            }
+            if (!values[++i].equals("n")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                parent.right = right;
+                q.add(right);
+            }
+        }
+        return root;
     }
 }
